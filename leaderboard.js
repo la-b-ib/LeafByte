5f87c186761aba6e902c72765d9548bdd35a77de
Labib Bin Shahed <103604925+la-b-ib@users.noreply.github.com>
2025-04-20 10:16:42 +0600

    Delete leaderboard.js

--- leaderboard.js
+++
@@ -1,8 +0,0 @@
-function updateLeaderboard(username, points) {
-    // Example of how you would update the leaderboard
-    let leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || [];
-    leaderboard.push({ username, points });
-    leaderboard = leaderboard.sort((a, b) => b.points - a.points);
-    localStorage.setItem('leaderboard', JSON.stringify(leaderboard));
-  }
-  
