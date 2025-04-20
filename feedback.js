2ba35111495ecb6e6591524c9b00898b3cc2a05c
Labib Bin Shahed <103604925+la-b-ib@users.noreply.github.com>
2025-04-20 10:16:57 +0600

    Delete feedback.js

--- feedback.js
+++
@@ -1,9 +0,0 @@
-function submitFeedback(feedback) {
-    const feedbackData = {
-      message: feedback,
-      timestamp: new Date().toISOString()
-    };
-    localStorage.setItem('feedback', JSON.stringify(feedbackData));
-    alert("Feedback submitted successfully!");
-  }
-  
